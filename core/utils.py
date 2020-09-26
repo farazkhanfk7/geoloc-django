df20bea13867b3d2fc2fe4cf1552086c041883b1
farazkhanfk7 <farazkhan138@gmail.com>
2020-09-26 19:09:36 +0530

    support for folium map

--- core/utils.py
+++ core/utils.py
@@ -7,3 +7,26 @@ def get_geo(ip):
     city = g.city(ip)
     lat, lon = g.lat_lon(ip)
     return country, city, lat, lon
+
+def get_center_coordinates(lat_a,lon_a,lat_b,lon_b):
+    cord = [(lat_a+lat_b)/2,(lon_a+lon_b)/2]
+    return cord
+
+def get_ip_address(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+def get_zoom_value(distance):
+    if distance <= 100:
+        return 8
+    elif distance>100 and distance<=5000:
+        return 4
+    else:
+        return 2
+
+    #will be using this to get proper map zoom value according to the distance.
+    #to be used in folium.Map
--- core/views.py
+++ core/views.py
@@ -4,7 +4,8 @@ from . models import Measurement
 from . forms import MeasurementForm
 from geopy.geocoders import Nominatim
 from geopy.distance import geodesic
-from .utils import get_geo
+from .utils import get_geo,get_center_coordinates,get_zoom_value,get_ip_address
+import folium
 
 # Create your views here.
 def home(request):
@@ -13,13 +14,18 @@ def home(request):
         form = MeasurementForm(request.POST or None)
         geolocator = Nominatim(user_agent='core')
 
+        #to get realtime ip-address once deployed
+        # ip = get_ip_address(request)
+        # print(ip)
+
         ip='139.167.230.131' #delhi central kotwali tehsil
-        country, city, lat, lon = get_geo(ip)
+        _country, city, lat, lon = get_geo(ip)
         # print(f'{city},{country} and lat-log:{lat}:{lon}')
 
         location = geolocator.geocode(city)
         # print(f'Detailed location via geocode : {location}')
 
+        #location coordinates
         l_lat = lat
         l_lon = lon
         point_a = (l_lat,l_lon)
@@ -29,24 +35,65 @@ def home(request):
             destination_ = form.cleaned_data.get('destination')
             destination = geolocator.geocode(destination_)
             # print(destination)
+
+            #destination coordinates
             d_lat = destination.latitude
             d_lon = destination.longitude
+            point_b = (d_lat,d_lon)
 
-            point_b = (d_lat,d_lon)  
-            distance = round(geodesic(point_a,point_b).km,2)         
+            #calculate distance  
+            distance = round(geodesic(point_a,point_b).km,2)
+
+            #revised folium map
+            center_coordinates = get_center_coordinates(l_lat,l_lon,d_lat,d_lon)
+            m = folium.Map(width=800,height=500,location=center_coordinates,zoom_start=get_zoom_value(distance))
+            #location marker
+            folium.Marker([l_lat,l_lon],tooltip='click here for more',popup=city['city'],icon=folium.Icon(color='purple')).add_to(m)
+            #destination marker
+            folium.Marker([d_lat,d_lon],tooltip='click here for more',popup=destination,icon=folium.Icon(color='red')).add_to(m)
+            #draw line
+            line = folium.PolyLine(locations=[point_a,point_b],weight=5,color='blue',popup=f'Distance: {distance} km')
+            m.add_child(line)
+            m = m._repr_html_()
+
+            #save to form         
             instance.location = location
             instance.distance = distance
             instance.save()
 
         context = {
             'distance':obj,
-            'form': form
+            'form': form,
+            'map':m
         }
 
-        return HttpResponse("ho gya save")
+        return render(request,'main.html',context)
     else:
+        geolocator = Nominatim(user_agent='core')
+        #to get realtime ip-address once deployed
+        # ip = get_ip_address(request)
+        # print(ip)
+        #getting initial location
+        ip='139.167.230.131' #delhi central kotwali tehsil
+        _country, city, lat, lon = get_geo(ip)
+        # print(f'{city},{country} and lat-log:{lat}:{lon}')
+
+        location = geolocator.geocode(city)
+        # print(f'Detailed location via geocode : {location}')
+
+        #location coordinates
+        l_lat = lat
+        l_lon = lon
+        point_a = (l_lat,l_lon)
+
+        #initial folium map
+        m = folium.Map(width=800,height=500,location=point_a,zoom_start=10)
+        folium.Marker([l_lat,l_lon],tooltip='click here for more',popup=city['city'],icon=folium.Icon(color='purple')).add_to(m)
+        m = m._repr_html_()
+
         form = MeasurementForm()
         context = {
-            'form': form
-        }
+            'form': form,
+            'map' : m
+        } 
         return render(request,'main.html',context)
